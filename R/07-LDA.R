af5325e96659439c1966fa5dcfbc14ed39cd55d2
ljnewediuk <j.newedi@gmail.com>
2022-08-07 16:50:58 -0400

    Update 07-LDA.R

--- R/07-LDA.R
+++ R/07-LDA.R
@@ -3,21 +3,31 @@ library(tidyverse)
 library(vegan)
 
 # Load data
-sample_dat <- readRDS('derived_data/uid_prop_use_samples.rds')
+# Cluster assignments
+clusts <- readRDS('derived_data/cluster_assignments_samples.rds')
+# Habitat data
+sample_dat <- readRDS('derived_data/uid_prop_use_samples.rds') %>%
+  # Get only samples from desired period
+  filter(TOD == 'night' & sample_sequence == 'after') %>%
+  # Join group data
+  left_join(clusts)
 
-lda_ord <- sample_dat %>% 
-  mutate(across(crop_prop:wet_prop, list(sc = scale)))
-  select(crop_prop:wet_prop) %>%  #columns containing habitat variables
-  mutate(forest=scale(forest),
-         crop=scale(crop),
-         etc=scale(etc)) #must scale and center variables for LDA and PCA
+lda_dat <- sample_dat %>% 
+  ungroup() %>%
+  # mutate(crop_sc = as.vector(scale(crop_prop)), forest_sc = as.vector(scale(forest_prop)))
+  mutate(across(anthro_prop:wet_prop, list(sc = function(x) as.vector(scale(x, center = T))))) %>%
+  select(group, anthro_prop_sc:wet_prop_sc)
 
-lda.mod<-lda(group~., data=data.lda)
+library(MASS)
+library(ggbiplot)
+
+lda.mod <- lda(group ~., data=lda_dat)
 lda.mod
 plot(lda.mod)
 
+#**** NOTE: not working for day/after; will need to figure out what's happening
 g <- ggbiplot(lda.mod, choices=c(1,2), var.scale=FALSE,
-              groups = as.factor(data.lda$group), ellipse = TRUE, 
+              groups = as.factor(lda_dat$group), ellipse = TRUE, 
               ellipse.prob = 0.95, circle = FALSE, var.axes=TRUE)
 # g <- g + scale_color_manual(name = '', values=c("black","green","orange","blue","darkturquoise","deeppink"))
 # g <- g + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
