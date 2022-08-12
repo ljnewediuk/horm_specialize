025935dfe3b11bd0b31e13b3d3e9a8e89772448a
ljnewediuk <j.newedi@gmail.com>
2022-08-12 17:19:30 -0400

    Rename

---
+++ R/09-GC-T3_Models.R
@@ -0,0 +1,64 @@
+
+library(tidyverse)
+
+horm_dat <- readRDS('derived_data/hormone_data.rds') %>% 
+  st_drop_geometry() %>% 
+  select(animal_ID, uid, cort_ng_g, t3_ng_g) %>% 
+  ungroup() %>%
+  mutate(across(cort_ng_g:t3_ng_g, function(x) as.vector(scale(x)))) %>%
+  distinct() %>%
+  mutate(yr = substr(animal_ID, 9, 12),
+         animal_ID = substr(animal_ID, 1, 7)) %>%
+  left_join(readRDS('derived_data/cluster_assignments_summer.rds'))
+
+# Plot scatter with linear model
+ggplot(horm_dat, aes(x = t3_ng_g, y = cort_ng_g)) + 
+  scale_colour_viridis_d() +
+  geom_point() +
+  geom_smooth(method = 'lm', col = 'black') +
+  theme(legend.position = 'none',
+        panel.background = element_rect(colour = 'black', fill = 'white'),
+        panel.grid = element_blank(),
+        plot.margin = unit(c(0.5, 0.5, 1, 1), 'cm'),
+        axis.text = element_text(size = 18, colour = 'black'),
+        axis.title.x = element_text(size = 18, colour = 'black', vjust = -4),
+        axis.title.y = element_text(size = 18, colour = 'black', vjust = 5))
+# Plot scatter between hormones and individual slopes
+ggplot(horm_dat, aes(x = t3_ng_g, y = cort_ng_g, group = animal_ID, col = group)) + 
+  scale_colour_viridis_d() +
+  geom_point() +
+  geom_smooth(method = 'lm', se = F)
+
+# Fit fixed effects and random effects models
+lm_mod <- lm(cort_ng_g ~ t3_ng_g, data = horm_dat)
+me_mod <- lme4::lmer(cort_ng_g ~ t3_ng_g + (1 + t3_ng_g | animal_ID), data = horm_dat)
+
+# Extract BLUPs from me model
+me_BLUPs <- lme4::ranef(me_mod)[[1]] %>%
+  rownames_to_column('animal_ID') %>%
+  rename('slope' = t3_ng_g)
+# Extract residuals from model into df
+me_resids <- data.frame(animal_ID = horm_dat$animal_ID,
+                        cort_ng_g = horm_dat$cort_ng_g,
+                        t3_ng_g = horm_dat$t3_ng_g,
+                        uid = horm_dat$uid,
+                        resids = resid(me_mod))
+
+# Get confidence intervals
+#   sd_(Intercept)|animal_ID = random intercept variation (sd)
+#   cor_t3_ng_g.(Intercept)|animal_ID = intercept/slope correlation
+#   sd_t3_ng_g|animal_ID = random slope variation (sd)
+#   sigma = variation in fixed effects
+#   t3_ng_g = fixed effect slope
+confint(lm_mod)
+confint(me_mod, oldNames = F)
+
+# Compare with AIC
+AIC(lm_mod)
+AIC(me_mod)
+
+# Save BLUPs and residuals
+saveRDS(me_BLUPs, 'results/me_BLUPs.rds')
+saveRDS(me_resids, 'results/me_residuals.rds')
+
+
